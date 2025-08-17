94c2138b9de4fa27b927ca8680719d1c9a7132b0
mjchapur <jc@bb.vision>
2025-08-16 21:10:59 -0300

    UPDATE database configuration

---
+++ backend/app/__init__.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+load_dotenv()
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    
+    db_url = os.getenv("DATABASE_URL")
+    if not db_url:
+        raise RuntimeError("DATABASE_URL no está configurada.")
+    
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    from . import models
+
+    @app.route("/")
+    def index():
+        return "¡El backend de Flask para IAM Manager está funcionando!"
+
+    @app.route("/api/health")
+    def health_check():
+        return jsonify({"status": "ok", "message": "API is healthy"}), 200
+
+    return app
