5c6bf166245aa8f8b16d08865a44123e3a46c0ca
damian <dam.lorenzo@hotmail.com>
2025-10-25 16:31:20 -0300

    Update settings.js

--- frontend/src/settings/settings.js
+++ frontend/src/settings/settings.js
@@ -1,6 +1,7 @@
 const home = '/'
 const managment = '/managment'
 const api_base_url = process.env.API_URL
-const api_users = api_base_url + "/"
+const api_base_storage = api_base_url + '/storage'
+const api_search = api_base_storage + "/search"
 
 export { home, managment }
