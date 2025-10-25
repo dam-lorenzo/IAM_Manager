28bbdd635be9a83c643dc02eb518e4af4807e5af
julieta chapur <juli.chapur@hotmail.com>
2025-10-25 17:51:34 -0300

    UPDATE: handling errors

--- frontend/src/components/Users/UsersTable.js
+++ frontend/src/components/Users/UsersTable.js
@@ -1,6 +1,5 @@
 // src/components/Users/UsersTable.js
 import React, { useState, useEffect } from 'react';
-import React from 'react';
 
 import {
   Container,
@@ -20,15 +19,51 @@ import { api_search_users } from '../../settings/settings';
 
 function UsersTable() {
   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchUsers = async () => {
-      const response = await fetch(api_search_users);
+      try { 
+        const response = await fetch(api_search_users);
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status}`);
+        }
+      
       const data = await response.json();
       setUsers(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
     };
     fetchUsers();
   }, []);
+
+  // const handleEdit = (userId) => {
+  //   // Aquí iría la lógica para abrir un modal de edición, por ejemplo
+  // };
+
+  // const handleDelete = (userId) => {
+  //   // Aquí iría la lógica para mostrar una confirmación y eliminar el usuario
+  // };
+  // handleAdd (?)
+  if (loading) {
+    return (
+      <Container sx={{ display: 'flex', justifyContent: 'center', mt: 4 }}>
+        <CircularProgress />
+      </Container>
+    );
+  }
+
+  if (error) {
+    return (
+      <Container sx={{ mt: 4 }}>
+        <Alert severity="error">empty database: {error}</Alert>
+      </Container>
+    );
+  }
   return (
     <Container sx={{ mt: 4 }}> {}
       <Typography variant="h4" gutterBottom>
