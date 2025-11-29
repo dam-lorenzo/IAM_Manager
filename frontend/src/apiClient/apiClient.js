68efe24c63c336f3d81fad7cf9ca0313169466aa
damian <dam.lorenzo@hotmail.com>
2025-11-29 16:54:51 -0300

    Create frontend/scr/apiClient/apiClient.js

---
+++ frontend/src/apiClient/apiClient.js
@@ -0,0 +1,12 @@
+// apiClient.js
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  timeout: 8000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default apiClient;
